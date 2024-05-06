168b5d265ee4e4fbcbb44a6561f3426984693d45
akpoco <atul.k@pococare.com>
2024-05-06 14:52:13 +0530

    role filter done

--- src/Dashboard/Components/Filters.jsx
+++ src/Dashboard/Components/Filters.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
+import React, { useState } from 'react'
+import MultipleSelect from './Multiselect'
 
-export const Filters = () => {
+export const Filters = ({ filters, setFilters }) => {
+    const roleOptions = [
+        {
+            label: 'Frontend Developer',
+            value: 'frontend'
+        },
+        {
+            label: 'Backend Developer',
+            value: 'backend'
+        },
+        {
+            label: 'Android Developer',
+            value: 'android'
+        },
+        {
+            label: 'IOS Developer',
+            value: 'ios'
+        },
+        {
+            label: 'Tech Lead',
+            value: 'tech lead'
+        }
+    ]
     return (
-        <div>Filters</div>
+        <Grid2>
+            <MultipleSelect options={roleOptions} value={filters} setValue={setFilters} label="Role" filterName='roles' />
+        </Grid2>
     )
 }
